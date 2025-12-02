a93d96efd259ac01c5619e41b130c4e565ac0172
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-02 18:55:27 +0530

    Register DbContext in Program.cs

--- InventoryOrderManagement.API/Program.cs
+++ InventoryOrderManagement.API/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Add PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
