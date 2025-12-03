ca25db9900dc8236637ff5191391aa682b31ec03
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-03 22:31:50 +0530

    update cate dto

---
+++ InventoryOrderManagement.API/Models/DTOs/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+namespace InventoryOrderManagement.API.Models.DTOs;
+
+public class UpdateCategoryDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
+
