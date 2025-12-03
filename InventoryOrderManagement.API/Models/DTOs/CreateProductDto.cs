a4bfb3a1355c2958f7a2d6db0f6984496386aa76
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-03 22:30:54 +0530

    change create prod dto

--- InventoryOrderManagement.API/Models/DTOs/CreateProductDto.cs
+++ InventoryOrderManagement.API/Models/DTOs/CreateProductDto.cs
@@ -3,5 +3,10 @@ namespace InventoryOrderManagement.API.Models.DTOs;
 public class CreateProductDto
 {
     public string Name { get; set; } = string.Empty;
-    public int Quantity { get; set; }
+    public string SKU { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int? CategoryId { get; set; }
+    public int? SupplierId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public bool IsActive { get; set; } = true;
 }
