a74618657bfe445896249a95179f52b3450bb767
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-03 22:35:42 +0530

    Add initial migration with database schema for categories, customers, roles, suppliers, warehouses, products, sales orders, purchase orders, and related entities

--- InventoryOrderManagement.API/Migrations/20251203152058_InitialCreate.Designer.cs
+++ InventoryOrderManagement.API/Migrations/20251203152058_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InventoryOrderManagement.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InventoryOrderManagement.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251203152058_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
