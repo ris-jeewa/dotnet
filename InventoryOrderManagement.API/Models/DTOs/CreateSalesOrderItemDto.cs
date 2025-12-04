d391f19447ca1cf2808241c1ba68ca461ec4aea5
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-05 00:17:55 +0530

    create 	salesorderitems dto

--- InventoryOrderManagement.API/Models/DTOs/CreateSalesOrderItemDto.cs
+++ InventoryOrderManagement.API/Models/DTOs/CreateSalesOrderItemDto.cs
@@ -2,6 +2,7 @@ namespace InventoryOrderManagement.API.Models.DTOs;
 
 public class CreateSalesOrderItemDto
 {
+    public int SalesOrderId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
