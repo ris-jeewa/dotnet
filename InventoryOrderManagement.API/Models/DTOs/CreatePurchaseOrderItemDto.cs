d112182fab5db08b0fa800982e40273d71d0e1f1
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-04 21:51:29 +0530

    create purchaseorder items dto

---
+++ InventoryOrderManagement.API/Models/DTOs/CreatePurchaseOrderItemDto.cs
@@ -0,0 +1,9 @@
+namespace InventoryOrderManagement.API.Models.DTOs;
+
+public class CreatePurchaseOrderItemDto
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
+
