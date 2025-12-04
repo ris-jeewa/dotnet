79655df000e5c452ac629cb2a8f3f88d8f93d608
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-04 21:21:27 +0530

    update warehouse dto

---
+++ InventoryOrderManagement.API/Models/DTOs/UpdateWarehouseDto.cs
@@ -0,0 +1,8 @@
+namespace InventoryOrderManagement.API.Models.DTOs;
+
+public class UpdateWarehouseDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Address { get; set; }
+}
+
