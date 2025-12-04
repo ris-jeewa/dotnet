26b44a4e00c1d5a2c17119050fd084e9ca6ee522
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-04 07:28:32 +0530

    create aand update supplier dtos

---
+++ InventoryOrderManagement.API/Models/DTOs/UpdateSupplierDto.cs
@@ -0,0 +1,10 @@
+namespace InventoryOrderManagement.API.Models.DTOs;
+
+public class UpdateSupplierDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? ContactPerson { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+}
